65a409a24ce5451dcb72aac3069aea3732edc30f
sybilng <73300419+sybilng@users.noreply.github.com>
2022-12-03 12:05:53 +0800

    new application

---
+++ webapp/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db=SQLAlchemy()
+DB_NAME = "database.db" 
+
+def create_app(): 
+   
+    app=Flask(__name__)
+    app.config['SECRET_KEY']='testsecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
+
+    from .views import views 
+    
+    app.register_blueprint(views,url_prefix='/')
+
+
+    if not path.exists('instance/'+ DB_NAME): 
+        with app.app_context():
+            db.create_all()
+            print(db)
+            print('Created Database')
+
+    return app 
+
---
+++ webapp/views.py
@@ -0,0 +1,8 @@
+
+from . import db
+from sqlalchemy.sql import func
+
+views=Blueprint("views",__name__)
+
+
+

e22b32afbdc1d6d68bda07138facc99558f86bfc
markwee190 <78016702+markwee190@users.noreply.github.com>
2022-12-03 12:30:32 +0800

    DB init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from  webapp import create_app
 app = create_app()
 
 if __name__ == '__main__':
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -13,16 +13,16 @@ def create_app():
     db.init_app(app)
     
 
-    from .views import views 
+    from .models import User, BankAccount, ScheduledTransactions
     
-    app.register_blueprint(views,url_prefix='/')
+    # app.register_blueprint(models,url_prefix='/')
 
 
-    if not path.exists('instance/'+ DB_NAME): 
-        with app.app_context():
-            db.create_all()
-            print(db)
-            print('Created Database')
+    # if not path.exists('instance/'+ DB_NAME): 
+    with app.app_context():
+        db.create_all()
+        print(db)
+        print('Created Datasbase')
 
     return app 
 

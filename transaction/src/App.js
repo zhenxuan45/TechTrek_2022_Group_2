c2544764c1985de97f21e1d0574d406d3b14a198
zhenxuan45 <zhenxuan45@hotmail.com>
2022-12-03 12:39:27 +0800

    Dashboard layout

--- transaction/src/App.js
+++ transaction/src/App.js
@@ -1,138 +1,17 @@
 
 import { useState, useEffect } from 'react'
-import AddTask from './components/AddTask';
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
 import axios from 'axios';
+import BankAccountType from './components/BankAccountType';
+import BankBalance from './components/BankBalance';
+import ScheduledTransactions from './components/ScheduledTransactions'
+import BasicTabs from './components/Tabs';
 
 
 function App() {
 
-  // tasks is part of the state, to change any component, need to use the defined function setTasks
-  // state is immutate, you can only create and add new
-  // state values should be kept in the top level components so that lower tiered components can be able to use them
-  // test server npm i json-server
-  // in the package.json set "server": "json-server -- watch db.json --port 5000" in scripts
-  const [showAddTask, setShowAddTask] = useState(false)
-
-  const [tasks, setTasks] = useState([])
-
-  // useEffect is used to get something to happen the moment the page loads
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
-    }
-
-    getTasks()
-  }, [])
-
-  // Fetch Tasks
-  const fetchTasks = async () => {
-
-    //axios is used here to avoid double promise
-    const res = await axios.get(`http://localhost:5000/tasks`)
-
-    return res.data
-  }
-
-  //Used to update  Reminder status
-  // Fetch Task
-  const fetchTask = async (id) => {
-
-    //axios is used here to avoid double promise
-    const res = await axios.get(`http://localhost:5000/tasks/${id}`)
-
-    return res.data
-  }
-
-  //Add Task
-  const addTask = async (task) => {
-    const res = await fetch('http://localhost:5000/tasks/', {
-      method: 'POST',
-      // Specifies the content type to be JSON
-      headers: {
-        'Content-type': 'application/json'
-      },
-      // Converts content to be JSON
-      body: JSON.stringify(task)
-    })
-
-    const data = await res.json()
-    setTasks([...tasks, data])
-
-    // // Generates a random ID
-    // const id = Math.floor (Math.random()*1000000 + 1 )
-    // //Appends the id to task
-    // const newTask = {id, ...task}
-    // // Add the task to existing task list
-    // setTasks([...tasks, newTask])
-  }
-
-  // Delete Task
-  const deleteTask = async (id) => {
-
-    //sends request to server to delete item
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE'
-    })
-
-    // Filter creates a new array with all elements that pass the test
-    // in this case, filters out task with id  == id
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
-
-  //Toggle Reminder
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id)
-    const updTask = {
-      ...taskToToggle, reminder: !taskToToggle.reminder
-    }
-
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      // Converts content to be JSON
-      body: JSON.stringify(updTask)
-    }
-    )
-    
-    const data = await res.json()
-
-    // Runs the setTasks command
-    setTasks(tasks.map((task) =>
-      // Runs throught the array and checks if the id returned is the same as the task.id
-      task.id === id ?
-
-        // IF SAME, spread the rest of the task but set reminder to opposite of the reminder
-        { ...task, reminder: data.reminder }
-
-        // IF NOT SAME, Do nothing
-        : task))
-  }
-
-
   return (
     <div className="container">
-      {/*  */}
-      <Header
-        title='Task Tracker'
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-
-      {/* sends the prop down to the component */}
-      {showAddTask && <AddTask onAdd={addTask} />}
-
-      {/* Check if task.length > 0 */}
-      {tasks.length > 0 ?
-        // IF TRUE, RUN THIS
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> :
-        // RUN THIS
-        "No Tasks to Show"
-      }
+      <BasicTabs/>
     </div>
   );
 }
---
+++ transaction/src/components/Tabs.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import BankAccountType from './BankAccountType';
+import BankBalance from './BankBalance';
+import ScheduledTransactions from './ScheduledTransactions'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider'}} display="flex" justifyContent="center" width="100%">
+        <Tabs variant="scrollable" scrollButtons="auto" value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Account Type" {...a11yProps(0)} />
+          <Tab label="Bank Balance" {...a11yProps(1)} />
+          <Tab label="Transactions" {...a11yProps(2)} />
+
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <BankAccountType/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Bank Balance
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Transactions
+      </TabPanel>
+    </Box>
+  );
+}

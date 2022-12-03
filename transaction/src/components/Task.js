efeeb3e6f222d5969137cd9021d8da10d6bd3470
Johnson-LM <tinyjohnsonluo@gmail.com>
2022-12-03 11:21:16 +0800

    Create Base Template for transactions

---
+++ transaction/src/components/Task.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
+    return (
+        <div
+            // IF task.reminder is true, output reminder, else nothing
+            className={`task ${task.reminder ? 'reminder' : ''}`}
+            onDoubleClick={() => onToggle(task.id)}
+        >
+            <h3>
+                {task.text} <FaTimes
+                    style={{ color: 'red', cursor: 'pointer' }}
+
+                    // using this empty function allows us to send the task.id back
+                    onClick={() => onDelete(task.id)}
+                />
+            </h3>
+            <p>
+                {task.day}
+            </p>
+        </div>
+    )
+}
+
+
+export default Task
+
+/*
+ How to change something in the main App.js from a component? 
+ Send a prop down from the main app 
+ State gets passed down, actions get passed up
+*/
